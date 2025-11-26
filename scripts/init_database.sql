f95293b9c75a6cca2693f99f8f44fa9edd687a85
Medazizkh <153307674+Medazizkh@users.noreply.github.com>
2025-11-26 21:39:24 +0100

    Create init_database.sql for DataWarehouse setup

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+--CREATE DATABASE 'DATAWAREHOUSE'
+
+
+USE master;
+create database DataWarehouse;
+
+
+
+USE DataWarehouse;
+
+
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
